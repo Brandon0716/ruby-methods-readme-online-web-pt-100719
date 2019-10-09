b28fc5c8a98aa1bf70283a8c85b754e09846c0e1
essential-robot-4364 <brandon.james414@gmail.com>
2019-10-09 17:12:05 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,4 +1,16 @@
-
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
 
 phrase = "Hello World!"
 puts phrase
